ab5513c18ec8689d9c31da525ab850d0bf23c1b1
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 02:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //your JS code here. If required.
 function flatten(value) {
- 
+  if (Array.isArray(value)) {
+    return value.reduce((acc, item) => acc.concat(flatten(item)), []);
+  } else if (typeof value === 'object' && value !== null) {
+    return Object.keys(value).reduce((acc, key) => {
+      return acc.concat({ [key]: flatten(value[key]) });
+    }, []);
+  } else {
+    return value;
+  }
 }
 
 module.exports=flatten;
